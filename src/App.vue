e2fce732e31391fe7d607948d56896f63de0267e
DUONG TIEN HIEU <tienhieuD@users.noreply.github.com>
2020-11-15 16:31:22 +0700

    Introduction

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-bind:title="message" v-if="seen">
+    Hover your mouse over me for a few seconds to see my dynamically bound
+    title!
+
+    <br />
+    {{ message }}
+    <br />
+    <input v-model="message" />
+    <br />
+    <ol v-on:click="addTodo">
+        <todo-item v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.id"/>
+    </ol>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoItem from './components/TodoItem.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { TodoItem },
+  name: "App",
+  data: () => ({
+    message: "You loaded this page on " + new Date().toLocaleString(),
+    seen: 1,
+    todos: [{id:1,  text: "Todo1" }, {id:2, text: "Todo2" }, {id:3, text: "Todo3" }],
+  }),
+  methods: {
+    addTodo: function (e) {
+      e.preventDefault();
+      console.log(this, e.preventDefault, e);
+      this.todos.push({ text: "todo xxx" });
+    },
+  },
+};
 </script>
 
 <style>
